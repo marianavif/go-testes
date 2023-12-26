7c533eb307694fde1218e2fe2beeec5aac0271af
Mariana Victor <marianavictorf@gmail.com>
2023-12-26 14:36:57 -0300

    feat: injecao de dependencia

---
+++ id.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func Cumprimenta(escritor io.Writer, nome string) {
+	fmt.Fprintf(escritor, "Olá, %s", nome)
+}
+
+func HandlerMeuCumprimento(w http.ResponseWriter, r *http.Request) {
+	Cumprimenta(w, "mundo")
+}
+
+func main() {
+	err := http.ListenAndServe(":5000", http.HandlerFunc(HandlerMeuCumprimento))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}

dd43c9ec19a95bf29001561c04765dd253c2b38a
Mariana Victor <marianavictorf@gmail.com>
2023-12-26 15:17:07 -0300

    feat: create spies for contagem function

--- mocks/contagem.go
+++ mocks/contagem.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	ultimaPalavra  = "Vai!"
+	inicioContagem = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SleeperSpy struct {
+	Chamadas int
+}
+
+type SleeperPadrao struct{}
+
+func (s *SleeperSpy) Sleep() {
+	s.Chamadas++
+}
+
+func (d *SleeperPadrao) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Contagem(saida io.Writer, sleeper Sleeper) {
+	for i := inicioContagem; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(saida, i)
+	}
+	sleeper.Sleep()
+	fmt.Fprint(saida, ultimaPalavra)
+}
+
 func main() {
-	Contagem()
+	sleeper := &SleeperPadrao{}
+	Contagem(os.Stdout, sleeper)
 }
--- mocks/contagem_test.go
+++ mocks/contagem_test.go
@@ -7,13 +7,21 @@ import (
 
 func TestContagem(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	sleeperSpy := &SleeperSpy{}
 
-	Contagem(buffer)
+	Contagem(buffer, sleeperSpy)
 
 	resultado := buffer.String()
-	esperado := "3"
+	esperado := `3
+2
+1
+Vai!`
 
 	if resultado != esperado {
 		t.Errorf("resultado '%s', esperado '%s' ", resultado, esperado)
+	}
+
+	if sleeperSpy.Chamadas != 4 {
+		t.Errorf("não houve chamadas suficientes do sleeper, esperado 4, resultado %d", sleeperSpy.Chamadas)
 	}
 }

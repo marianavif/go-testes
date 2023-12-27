1184c624364398d43834afa033399d28cb2146cb
Mariana Victor <marianavictorf@gmail.com>
2023-12-27 10:07:51 -0300

    test: change tests and spies

--- mocks/contagem.go
+++ mocks/contagem.go
@@ -7,39 +7,71 @@ import (
 	"time"
 )
 
+// constants
+
 const (
 	ultimaPalavra  = "Vai!"
 	inicioContagem = 3
+	escrita        = "escrita"
+	pausa          = "pausa"
 )
 
+// types
+
 type Sleeper interface {
-	Sleep()
+	Pausa()
 }
 
-type SleeperSpy struct {
-	Chamadas int
+type SpyContagemOperacoes struct {
+	Chamadas []string
 }
 
-type SleeperPadrao struct{}
-
-func (s *SleeperSpy) Sleep() {
-	s.Chamadas++
+type SleeperConfiguravel struct {
+	duracao time.Duration
+	pausa   func(time.Duration)
 }
 
-func (d *SleeperPadrao) Sleep() {
-	time.Sleep(1 * time.Second)
+type TempoSpy struct {
+	duracaoPausa time.Duration
 }
+
+// TempoSpy methods
+
+func (t *TempoSpy) Pausa(duracao time.Duration) {
+	t.duracaoPausa = duracao
+}
+
+// SleeperConfiguravel methods
+
+func (s *SleeperConfiguravel) Pausa() {
+	s.pausa(s.duracao)
+}
+
+// SpyContagemOperacoes methods
+
+func (s *SpyContagemOperacoes) Pausa() {
+	s.Chamadas = append(s.Chamadas, pausa)
+}
+
+func (s *SpyContagemOperacoes) Write(p []byte) (n int, err error) {
+	s.Chamadas = append(s.Chamadas, escrita)
+	return
+}
+
+// functions
 
 func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
-		sleeper.Sleep()
+		sleeper.Pausa()
 		fmt.Fprintln(saida, i)
 	}
-	sleeper.Sleep()
+	sleeper.Pausa()
 	fmt.Fprint(saida, ultimaPalavra)
 }
 
+// main function
+
 func main() {
-	sleeper := &SleeperPadrao{}
+	sleeper := &SleeperConfiguravel{1 * time.Second, time.Sleep}
 	Contagem(os.Stdout, sleeper)
 }
--- mocks/contagem_test.go
+++ mocks/contagem_test.go
@@ -2,26 +2,55 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestContagem(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	sleeperSpy := &SleeperSpy{}
+	t.Run("imprime 3 até Vai!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Contagem(buffer, &SpyContagemOperacoes{})
 
-	Contagem(buffer, sleeperSpy)
-
-	resultado := buffer.String()
-	esperado := `3
+		resultado := buffer.String()
+		esperado := `3
 2
 1
 Vai!`
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s' ", resultado, esperado)
-	}
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s' ", resultado, esperado)
+		}
+	})
+	t.Run("pausa antes de cada impressão", func(t *testing.T) {
+		spyImpressoraSleep := &SpyContagemOperacoes{}
+		Contagem(spyImpressoraSleep, spyImpressoraSleep)
 
-	if sleeperSpy.Chamadas != 4 {
-		t.Errorf("não houve chamadas suficientes do sleeper, esperado 4, resultado %d", sleeperSpy.Chamadas)
+		esperado := []string{
+			pausa,
+			escrita,
+			pausa,
+			escrita,
+			pausa,
+			escrita,
+			pausa,
+			escrita,
+		}
+
+		if !reflect.DeepEqual(esperado, spyImpressoraSleep.Chamadas) {
+			t.Errorf("esperado %v chamadas, resultado %v", esperado, spyImpressoraSleep)
+		}
+	})
+}
+
+func TestSleeperConfiguravel(t *testing.T) {
+	tempoPausa := 5 * time.Second
+
+	tempoSpy := TempoSpy{}
+	sleeper := SleeperConfiguravel{tempoPausa, tempoSpy.Pausa}
+	sleeper.Pausa()
+
+	if tempoSpy.duracaoPausa != tempoPausa {
+		t.Errorf("deveria ter pausado por %v, mas pausou por %v", tempoPausa, tempoSpy.duracaoPausa)
 	}
 }
